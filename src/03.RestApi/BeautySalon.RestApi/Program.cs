138741756ad6fa2c2e6b9ef58506521f820fb93b
Reza7476 <rdehghani.akorn@gmail.com>
2025-10-14 18:02:56 +0330

    test 2

--- src/03.RestApi/BeautySalon.RestApi/Program.cs
+++ src/03.RestApi/BeautySalon.RestApi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac.Extensions.DependencyInjection;
+using BeautySalon.Common.Interfaces;
 using BeautySalon.infrastructure;
 using BeautySalon.RestApi.Configurations.Autofacs;
 using BeautySalon.RestApi.Configurations.ConnectionStrings;
@@ -6,6 +7,7 @@ using BeautySalon.RestApi.Configurations.Exceptions;
 using BeautySalon.RestApi.Configurations.JwtConfigs;
 using BeautySalon.RestApi.Configurations.RegisterAdmin;
 using BeautySalon.RestApi.Configurations.SwaggerConfigurations;
+using BeautySalon.RestApi.Implementations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,24 +16,24 @@ var (configuration, connectionString) = ConnectionStringConfig.LoadConfigAndConn
     builder.Environment.EnvironmentName,
     builder.Environment.ContentRootPath);
 
-//builder.Services.AddJwtAuthentication(
-//    builder.Environment.EnvironmentName,
-//    builder.Environment.ContentRootPath);
-
-
-
 builder.Configuration.AddConfiguration(configuration);
+
 builder.Host.AddAutofac();
 
 builder.Services.AddDbContext<EFDataContext>(options =>
     options.UseSqlServer(connectionString));
 
+builder.Services.AddSingleton<AdminInitializer>();
+builder.Services.AddHostedService<AdminInitializerHostedService>();
+
+builder.Services.AddSingleton<IJwtSettingService>(sp =>
+    new JwtSettingImplementation(builder.Environment.EnvironmentName, builder.Environment.ContentRootPath));
+
+builder.Services.AddJwtAuthentication();
+
 builder.Services.AddControllers();
 builder.Services.AddSwaggerConfigGen();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSingleton<AdminInitializer>();
-builder.Services.AddHostedService<AdminInitializerHostedService>(); 
 
 var app = builder.Build();
 
@@ -51,8 +53,10 @@ app.MapGet("/", context =>
 
 app.UseStaticFiles();
 app.UseHttpsRedirection();
+
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
