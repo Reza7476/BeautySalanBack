ccb802d2473fa3b35d468b3ac2d9c9a34d7b9de4
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-23 14:39:11 +0330

    get all review comments

--- src/01.core/BeautySalon.Services/AppointmentReviews/Contracts/Dtos/GetAllPublishedReviewsDto.cs
+++ src/01.core/BeautySalon.Services/AppointmentReviews/Contracts/Dtos/GetAllPublishedReviewsDto.cs
@@ -1,12 +1,10 @@
 ﻿namespace BeautySalon.Services.AppointmentReviews.Contracts.Dtos;
-public class GetAllReviewsDto
+public class GetAllPublishedReviewsDto
 {
     public string TreatmentTitle { get; set; } = default!;
     public string? Comment { get; set; }
     public DateTime CreatedAt { get; set; }
     public string? Name { get; set; }
     public string? LastName { get; set; }
-    public bool IsPublished { get; set; }
     public byte Rate { get; set; }
-    public string Id { get; set; } = default!;
 }
