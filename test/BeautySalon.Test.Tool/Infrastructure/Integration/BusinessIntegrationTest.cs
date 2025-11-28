8dc9a4b8f597b9da346c2a158aa44a61a43017e9
Reza7476 <rdehghani.akorn@gmail.com>
2025-11-28 10:47:14 +0330

    fix-integration test config

--- test/BeautySalon.Test.Tool/Infrastructure/Integration/BusinessIntegrationTest.cs
+++ test/BeautySalon.Test.Tool/Infrastructure/Integration/BusinessIntegrationTest.cs
@@ -1,39 +1,59 @@
-﻿using BeautySalon.infrastructure;
+﻿
+using BeautySalon.infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
-using System.Transactions;
 
 namespace BeautySalon.Test.Tool.Infrastructure.Integration;
 
-public class BusinessIntegrationTest : EFDataContextDatabaseFixture
+public class BusinessIntegrationTest : IDisposable
 {
-    protected EFDataContext DbContext { get; set; }
+    protected EFDataContext DbContext { get;  set; }
 
     public BusinessIntegrationTest()
     {
         DbContext = CreateDataContext();
+        BeginTransaction();
     }
 
-    protected void Save<T>(params T[] entities)
-      where T : class
+    private IDbContextTransaction _transaction;
+
+    private void BeginTransaction()
+    {
+        _transaction = DbContext.Database.BeginTransaction();
+    }
+
+    protected void Save<T>(params T[] entities) where T : class
     {
         foreach (var entity in entities)
         {
-            DbContext.Save(entity);
+            DbContext.Set<T>().Add(entity);
+        }
+        DbContext.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            _transaction.Rollback(); 
+        }
+        finally
+        {
+            _transaction.Dispose();
+            DbContext.Dispose();
         }
     }
-}
 
-public class EFDataContextDatabaseFixture : DataBaseFixture
-{
-    protected static EFDataContext CreateDataContext()
+    private static EFDataContext CreateDataContext()
     {
         var settings = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("testAppSettings.json", true, false)
+            .AddJsonFile("testAppSettings.json", optional: true, reloadOnChange: false)
             .AddEnvironmentVariables()
             .AddCommandLine(Environment.GetCommandLineArgs())
             .Build();
+
         var testSettings = new InfrastructureConfig();
         settings.Bind("PersistenceConfig", testSettings);
 
@@ -47,22 +67,4 @@ public class EFDataContextDatabaseFixture : DataBaseFixture
 public class InfrastructureConfig
 {
     public string ConnectionString { get; set; } = default!;
-
-}
-
-
-public class DataBaseFixture : IDisposable
-{
-    private readonly TransactionScope _transactionScope;
-
-    public DataBaseFixture()
-    {
-        _transactionScope = new TransactionScope(
-            TransactionScopeOption.Required,
-            TransactionScopeAsyncFlowOption.Enabled);
-    }
-    public void Dispose()
-    {
-        _transactionScope?.Dispose();
-    }
 }
